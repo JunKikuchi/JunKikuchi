084e3090727bd269ab102e2a21b86e640e16057d
Kikuchi Jun <kikuchi@bonnou.com>
2020-03-29 23:26:17 +0900

    駒移動時の引数に経過秒数を追加

--- src/ShogiX/Shogi.hs
+++ src/ShogiX/Shogi.hs
@@ -1,13 +1,14 @@
 module ShogiX.Shogi where
 
 import           ShogiX.Shogi.Types
+import           ShogiX.Clocks                  ( Sec )
 
 -- | 平手作成
 hirate :: Shogi
 hirate = undefined
 
 -- | 駒の移動
-move :: Move -> Shogi -> Shogi
+move :: Sec -> Move -> Shogi -> Shogi
 move = undefined
 
 -- | 駒の移動範囲を取得
