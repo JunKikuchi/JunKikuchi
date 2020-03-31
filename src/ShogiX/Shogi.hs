22a1f8d95ca5a96bad3f0a53d55cc57a94d67f18
Kikuchi Jun <kikuchi@bonnou.com>
2020-03-31 09:16:41 +0900

    駒の移動先 doctest 追記

--- src/ShogiX/Shogi.hs
+++ src/ShogiX/Shogi.hs
@@ -19,5 +19,18 @@ move :: Sec -> Move -> Shogi -> Shogi
 move = undefined
 
 -- | 駒の移動範囲を取得
+--
+-- >>> import qualified Data.Map.Strict as Map
+-- >>> import qualified Data.List.NonEmpty as NonEmpty
+-- >>> import qualified ShogiX.Clocks as Clocks
+-- >>>
+-- >>> let board = Board (Map.fromList [((F5, R5), Piece Black Pawn)])
+-- >>> let stands = Stands [] []
+-- >>> let position = Position Black board stands Clocks.infinity
+-- >>> let positions = Positions (NonEmpty.fromList [position])
+-- >>>
+-- >>> let shogi = Shogi Open positions
+-- >>> movables shogi
+-- fromList [((F5,R5), fromList [((F5, R4),No)]]
 movables :: Shogi -> Movables
 movables = undefined
