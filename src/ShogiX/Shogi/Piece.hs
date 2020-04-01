29807c5a864d86532ae8da3f159041b82d6864ec
Kikuchi Jun <kikuchi@bonnou.com>
2020-04-01 21:19:27 +0900

    関数名等を変更してドキュメントを追記

--- src/ShogiX/Shogi/Piece.hs
+++ src/ShogiX/Shogi/Piece.hs
@@ -7,7 +7,7 @@ import           ShogiX.Shogi.Types
 -- | 駒の移動先を取得
 movable :: Piece -> SrcSquare -> Map Square Color -> Movable
 movable piece s sc = case pieceType piece of
-  Pawn           -> buildPromo $ pawn s
+  Pawn           -> promoMovables $ pawn s
   Lance          -> undefined
   Knight         -> undefined
   Silver         -> undefined
@@ -22,35 +22,43 @@ movable piece s sc = case pieceType piece of
   PromotedBishop -> undefined
   PromotedRook   -> undefined
  where
-  buildPromo = build promo color sc
-  color      = pieceColor piece
+  promoMovables = movables promo color sc
+  color         = pieceColor piece
 
-type Movements = [[DestSquare]]
+-- | 駒の可動範囲リスト
+type Movements = [Movement]
+
+-- | 駒の可動範囲
+type Movement  = [DestSquare]
+
+-- | 成り不成判定
 type Promo = Square -> Promotable
 
-build :: Promo -> Color -> Map Square Color -> Movements -> Movable
-build p c sc mss = Map.fromList [ (m, p m) | ms <- mss, m <- hit c sc ms ]
+-- | 駒の可動範囲を生成
+movables :: Promo -> Color -> Map Square Color -> Movements -> Movable
+movables p c sc mss =
+  Map.fromList [ (m, p m) | ms <- mss, m <- takeMovables c sc ms ]
 
 -- | 駒があるマス目まで可動範囲を刈り取る
 --
--- >>> hit Black Map.empty [(F5, R3), (F5, R2), (F5, R1)]
+-- >>> takeMovables Black Map.empty [(F5, R3), (F5, R2), (F5, R1)]
 -- [(F5,R3),(F5,R2),(F5,R1)]
 --
--- >>> hit Black (Map.fromList [((F5, R2), Black)]) [(F5, R3), (F5, R2), (F5, R1)]
+-- >>> takeMovables Black (Map.fromList [((F5, R2), Black)]) [(F5, R3), (F5, R2), (F5, R1)]
 -- [(F5,R3)]
 --
--- >>> hit Black (Map.fromList [((F5, R2), White)]) [(F5, R3), (F5, R2), (F5, R1)]
+-- >>> takeMovables Black (Map.fromList [((F5, R2), White)]) [(F5, R3), (F5, R2), (F5, R1)]
 -- [(F5,R3),(F5,R2)]
 --
--- >>> hit Black Map.empty []
+-- >>> takeMovables Black Map.empty []
 -- []
-hit :: Color -> Map Square Color -> [Square] -> [Square]
-hit _ _  []       = []
-hit c sc (s : ss) = case Map.lookup s sc of
+takeMovables :: Color -> Map Square Color -> [Square] -> [Square]
+takeMovables _ _  []       = []
+takeMovables c sc (s : ss) = case Map.lookup s sc of
   Just color -> if color == c then [] else [s]
-  Nothing    -> s : hit c sc ss
+  Nothing    -> s : takeMovables c sc ss
 
--- | 成り不成
+-- | 成り不成判定
 promo :: Promo
 promo (_, R1) = Must
 promo (_, R2) = Option
