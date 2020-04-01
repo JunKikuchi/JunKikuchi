9ea984c418eb72a77dfea980518b824c29e6fe26
Kikuchi Jun <kikuchi@bonnou.com>
2020-04-01 22:08:52 +0900

    駒の移動先作成時に先手後手を考慮するように修正

--- src/ShogiX/Shogi/Piece.hs
+++ src/ShogiX/Shogi/Piece.hs
@@ -5,9 +5,16 @@ import qualified RIO.Map                       as Map
 import           ShogiX.Shogi.Types
 
 -- | 駒の移動先を取得
+--
+-- >>> import RIO.Map as Map
+-- >>> movable (Piece Black Pawn) (F5, R5) (Map.fromList [((F5, R5), Black)])
+-- Movable {unMovable = fromList [((F5,R4),No)]}
+--
+-- >>> movable (Piece White Pawn) (F5, R5) (Map.fromList [((F5, R5), Black)])
+-- Movable {unMovable = fromList [((F5,R6),No)]}
 movable :: Piece -> SrcSquare -> Map Square Color -> Movable
 movable piece s sc = case pieceType piece of
-  Pawn           -> promoMovables $ pawn s
+  Pawn           -> promoMovables $ pawn color s
   Lance          -> undefined
   Knight         -> undefined
   Silver         -> undefined
@@ -67,10 +74,13 @@ promo _       = No
 
 -- | 歩兵の可動範囲
 --
--- >>> pawn (F5, R5)
+-- >>> pawn Black (F5, R5)
 -- [[(F5,R4)]]
-pawn :: SrcSquare -> Movements
-pawn s = [one $ up s]
+--
+-- >>> pawn White (F5, R5)
+-- [[(F5,R6)]]
+pawn :: Color -> SrcSquare -> Movements
+pawn c s = [one $ forward c s]
 
 -- | マス目リストから最初のひとつ取得
 --
@@ -82,6 +92,17 @@ pawn s = [one $ up s]
 one :: Movement -> Movement
 one = take 1
 
+-- | 前方マス目リスト生成
+--
+-- >>> forward Black (F1, R5)
+-- [(F1,R4),(F1,R3),(F1,R2),(F1,R1)]
+--
+-- >>> forward White (F1, R5)
+-- [(F1,R6),(F1,R7),(F1,R8),(F1,R9)]
+forward :: Color -> Square -> Movement
+forward Black = up
+forward White = down
+
 -- | 上へのマス目リスト生成
 --
 -- >>> up (F1, R5)
@@ -91,3 +112,13 @@ one = take 1
 -- []
 up :: Square -> Movement
 up (file, rank) = [ (file, r) | r <- drop 1 $ reverse [minBound .. rank] ]
+
+-- | 下へのマス目リスト生成
+--
+-- >>> down (F1, R5)
+-- [(F1,R6),(F1,R7),(F1,R8),(F1,R9)]
+--
+-- >>> down (F1, R9)
+-- []
+down :: Square -> Movement
+down (file, rank) = [ (file, r) | r <- drop 1 [rank .. maxBound] ]
