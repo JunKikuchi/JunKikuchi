51e173962d5b1aac34e473185851847852347a76
Kikuchi Jun <kikuchi@bonnou.com>
2020-04-11 00:52:16 +0900

    駒の移動範囲から王手になるものを取り除いた

--- src/ShogiX/Shogi/Position.hs
+++ src/ShogiX/Shogi/Position.hs
@@ -1,15 +1,39 @@
 module ShogiX.Shogi.Position where
 
+import           RIO
+import qualified RIO.Map                       as Map
 import           ShogiX.Shogi.Types
 import qualified ShogiX.Shogi.Board            as Board
 import qualified ShogiX.Shogi.Stand            as Stand
 
 -- | 駒の移動範囲を取得
 movables :: Position -> Movables
-movables pos = Board.movables turn board
+movables pos = removeCheckedMovables turn board ms
  where
   turn  = positionTurn pos
   board = positionBoard pos
+  ms    = Board.movables turn board
+
+-- | 駒の可動範囲から王手になるものを除く
+-- >>> removeCheckedMovables Black (Board (Map.fromList [((F5, R9), Piece Black King), ((F5, R8), Piece White Gold)])) (Movables (Map.fromList [((F5, R9), Movable (Map.fromList [((F5, R8), No), ((F4, R9), No)]))]))
+-- Movables {unMovables = fromList [((F5,R9),Movable {unMovable = fromList [((F5,R8),No)]})]}
+removeCheckedMovables :: Color -> Board -> Movables -> Movables
+removeCheckedMovables turn board =
+  Movables
+    . Map.filter (not . Map.null . unMovable)
+    . Map.mapWithKey (removeCheckedMovable turn board)
+    . unMovables
+
+-- | 駒の可動範囲から王手になるものを除く
+-- >>> removeCheckedMovable Black (Board (Map.fromList [((F5, R9), Piece Black King), ((F5, R8), Piece White Gold)])) (F5, R9) (Movable (Map.fromList [((F5, R8), No), ((F4, R9), No)]))
+-- Movable {unMovable = fromList [((F5,R8),No)]}
+removeCheckedMovable :: Color -> Board -> SrcSquare -> Movable -> Movable
+removeCheckedMovable turn board src =
+  Movable . Map.filterWithKey isNotChecked . unMovable
+ where
+  isNotChecked dest _ = isJust $ do
+    mv <- Board.move src False dest board
+    guard $ not . Board.checked turn . fst $ mv
 
 -- | 持ち駒の打ち先範囲を取得
 droppables :: Position -> Droppables
---
+++ test-tasty/Test/ShogiX/Shogi/Position.hs
@@ -0,0 +1,122 @@
+module Test.ShogiX.Shogi.Position where
+
+import           RIO
+import qualified RIO.Map                       as Map
+import           Test.Tasty
+import           Test.Tasty.Hspec
+import           ShogiX.Shogi.Types
+import qualified ShogiX.Shogi.Position         as Position
+import qualified ShogiX.Shogi.Stand            as Stand
+import           ShogiX.Clocks                 as Clocks
+
+{-# ANN module "HLint: ignore Use camelCase" #-}
+spec_movables :: Spec
+spec_movables = describe "movables" $ do
+  describe "王手されていない場合" $ do
+    it "可動範囲を返す"
+      $          Position.movables
+                   (Position
+                     Black
+                     (Board
+                       (Map.fromList
+                         [((F5, R9), Piece Black Pawn), ((F5, R1), Piece White Pawn)]
+                       )
+                     )
+                     (Stands Stand.empty Stand.empty)
+                     Clocks.infinity
+                   )
+      `shouldBe` Movables
+                   (Map.fromList
+                     [((F5, R9), Movable (Map.fromList [((F5, R8), No)]))]
+                   )
+    describe "先手"
+      $          it "可動範囲を返す"
+      $          Position.movables
+                   (Position
+                     Black
+                     (Board
+                       (Map.fromList
+                         [((F5, R9), Piece Black King), ((F5, R1), Piece White King)]
+                       )
+                     )
+                     (Stands Stand.empty Stand.empty)
+                     Clocks.infinity
+                   )
+      `shouldBe` Movables
+                   (Map.fromList
+                     [ ( (F5, R9)
+                       , Movable
+                         (Map.fromList
+                           [ ((F6, R8), No)
+                           , ((F5, R8), No)
+                           , ((F4, R8), No)
+                           , ((F6, R9), No)
+                           , ((F4, R9), No)
+                           ]
+                         )
+                       )
+                     ]
+                   )
+    describe "後手"
+      $          it "可動範囲を返す"
+      $          Position.movables
+                   (Position
+                     White
+                     (Board
+                       (Map.fromList
+                         [((F5, R9), Piece Black King), ((F5, R1), Piece White King)]
+                       )
+                     )
+                     (Stands Stand.empty Stand.empty)
+                     Clocks.infinity
+                   )
+      `shouldBe` Movables
+                   (Map.fromList
+                     [ ( (F5, R1)
+                       , Movable
+                         (Map.fromList
+                           [ ((F6, R2), No)
+                           , ((F5, R2), No)
+                           , ((F4, R2), No)
+                           , ((F6, R1), No)
+                           , ((F4, R1), No)
+                           ]
+                         )
+                       )
+                     ]
+                   )
+  describe "王手されている場合" $ do
+    describe "先手"
+      $          it "王手回避する可動範囲を返す"
+      $          Position.movables
+                   (Position
+                     Black
+                     (Board
+                       (Map.fromList
+                         [((F5, R9), Piece Black King), ((F5, R8), Piece White Gold)]
+                       )
+                     )
+                     (Stands Stand.empty Stand.empty)
+                     Clocks.infinity
+                   )
+      `shouldBe` Movables
+                   (Map.fromList
+                     [((F5, R9), Movable (Map.fromList [((F5, R8), No)]))]
+                   )
+    describe "後手"
+      $          it "王手回避する可動範囲を返す"
+      $          Position.movables
+                   (Position
+                     White
+                     (Board
+                       (Map.fromList
+                         [((F5, R1), Piece White King), ((F5, R2), Piece Black Gold)]
+                       )
+                     )
+                     (Stands Stand.empty Stand.empty)
+                     Clocks.infinity
+                   )
+      `shouldBe` Movables
+                   (Map.fromList
+                     [((F5, R1), Movable (Map.fromList [((F5, R2), No)]))]
+                   )
