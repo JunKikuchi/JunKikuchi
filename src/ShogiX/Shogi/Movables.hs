06b244d823b3340c23443d3e6bf34c4096cef8ee
Kikuchi Jun <kikuchi@bonnou.com>
2020-04-20 10:11:02 +0900

    駒の移動先有無チェックと移動先マス目セット関数を実装

--- src/ShogiX/Shogi/Movables.hs
+++ src/ShogiX/Shogi/Movables.hs
@@ -1,11 +1,14 @@
 module ShogiX.Shogi.Movables
   ( empty
   , fromList
+  , ShogiX.Shogi.Movables.null
+  , destSquareSet
   )
 where
 
 import           RIO
 import qualified RIO.Map                       as Map
+import qualified RIO.Set                       as Set
 import           ShogiX.Shogi.Types
 import qualified ShogiX.Shogi.Movable          as Movable
 
@@ -16,3 +19,15 @@ empty = Movables Map.empty
 -- | リストから駒の移動先を作成
 fromList :: [(SrcSquare, [(DestSquare, Promotable)])] -> Movables
 fromList = Movables . Map.fromList . map (second Movable.fromList)
+
+-- | 移動先の有無
+null :: Movables -> Bool
+null = Map.null . unMovables
+
+-- | 駒の移動先マス目セット作成
+destSquareSet :: Movables -> Set DestSquare
+destSquareSet =
+  Set.unions
+    . fmap (Set.fromList . Map.keys . unMovable)
+    . Map.elems
+    . unMovables
